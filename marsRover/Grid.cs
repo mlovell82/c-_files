f88b9d48f0189e8596671a984eeed36817df3902
Mick Lovell <mlovell@kent.edu>
2018-01-25 23:49:26 -0500

    Update Grid.cs

--- marsRover/Grid.cs
+++ marsRover/Grid.cs
@@ -6,65 +6,21 @@ using System.Threading.Tasks;
 
 namespace MickMarsRover
 {
-    public class Grid
-    {
-        private int length = 0;
-        private int width = 0;
-
-        public Grid(int enteredLength, int enteredWidth)
-        {
-            length = enteredLength;
-            width = enteredWidth;
-        }
-        public void getSize()
-        {
-            Console.WriteLine("length = " + length + " width = " + width);
-        }
-        
-        public void gridInterface()
-        {
-            string gridDimensionsEntry = Console.ReadLine();
-            string xAxis = "empty", yAxis = "empty";
-            bool xStringDone = false;
-            
-            for (int i = 0; i < gridDimensionsEntry.Length; ++i)
-            {
-                if(gridDimensionsEntry[i] == ' ')
-                {
-                    xStringDone = true;
-                    ++i;
-                }
-                if(!xStringDone)
-                {
-                    //handler for if gridDimensionEntry[i] is not an int
-                    if (xAxis == "empty")
-                    {
-                        xAxis = gridDimensionsEntry[i].ToString();
-                    }
-                    else
-                    {
-                        xAxis = xAxis + gridDimensionsEntry[i];
-                    }
-
-                }
-                if (xStringDone)
-                {
-                    //handler for if gridDimensionEntry[i] is not an int
-                    if (yAxis == "empty")
-                    {
-                        yAxis = gridDimensionsEntry[i].ToString();
-                    }
-                    else
-                    {
-                        yAxis = yAxis + gridDimensionsEntry[i];
-                    }
-                }
-
-            }
-            width = Int32.Parse(xAxis);
-            length = Int32.Parse(yAxis);
-
+    public class Grid{
+        private static int _length = 0;
+        private static int _width = 0;
+      
+        public static int getLength(){
+            return _length;
         }
 
+        public static int getWidth(){
+            return _width;
+        }
+
+        public void setGridDimensions(int newWidth, int newLength){
+            _width = newWidth;
+            _length = newLength;
+        }  
     }
 }
